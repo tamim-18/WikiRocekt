905b49da8c38ded90d97e090b32e1cdfb829f788
tamim-18 <tamim11903060@gmail.com>
2023-11-02 13:56:55 +0600

    l01

---
+++ app/[searchTerm]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import getWikiResults from "@/lib/getWikiResults";
+type Props = {
+  params: {
+    searchTerm: string;
+  };
+};
+
+export default async function SearchResults({ params: { searchTerm } }: Props) {
+  const wikiData: any = getWikiResults(searchTerm);
+  const data = await wikiData;
+  const results: Result[] | undefined = data?.query?.pages;
+  const content = (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen"></main>
+  );
+
+  return <div>page</div>;
+}
